e1dd09018b5dfb3ccfb9dedea330ae9eadfacd93
mike-p-blaize <mike.p@blaize.tech>
2023-05-02 20:47:18 +0300

    Rework pagination for reports route

--- src/report/report.service.ts
+++ src/report/report.service.ts
@@ -1,40 +1,66 @@
-import { Injectable } from '@nestjs/common';
-import { IReportResponse, IReportStruct } from './types';
+import { BadRequestException, Injectable } from '@nestjs/common';
+import { IReportJson, IReportResponse, IReportStruct } from './types';
 import { readFileSync } from 'fs';
 import * as path from 'path';
 import { assertParse } from 'typia';
+import { hashCode } from '../utils';
 
 @Injectable()
 export class ReportService {
-  private reports: Array<IReportResponse>;
-  private limit = 100;
+  private reports: Array<IReportJson>;
+  private perPage = 10;
+
+  private total;
+  private totalPages;
+
   constructor() {
     const filePath = path.join('storage', 'reports.json');
     const rawData = readFileSync(filePath, 'utf-8');
-    this.reports = assertParse<Array<IReportResponse>>(rawData);
+    this.reports = assertParse<Array<IReportJson>>(rawData);
+    this.total = this.reports.length;
+    this.totalPages = Math.ceil(this.total / this.perPage);
   }
-  getReport(skip: number): IReportStruct {
-    const reports = JSON.parse(JSON.stringify(this.reports));
-    const total = this.reports.length;
-    const end = skip + this.limit < total ? skip + this.limit : total;
-    const sliced = reports.slice(skip, end).map((v) => {
+
+  private getReports(): Array<IReportJson> {
+    /*structuredClone(this.reports);*/
+    return JSON.parse(JSON.stringify(this.reports));
+  }
+
+  getByPage(page: number): IReportResponse {
+    if (page > this.totalPages || page < 1)
+      throw new BadRequestException("page doesn't exist");
+
+    const reports = this.getReports();
+
+    const end = this.perPage * page;
+    const start = end - this.perPage;
+
+    const sliced = reports.slice(start, end).map((v): IReportStruct => {
       const reportDateTimestamp = new Date(v.reportDate).valueOf();
       const publicationDateTimestamp = new Date(v.publicationDate).valueOf();
       //TBD: date/time format
-      v.reportDate = !isNaN(reportDateTimestamp)
+      const reportDate = !isNaN(reportDateTimestamp)
         ? reportDateTimestamp.toString()
         : '';
-      v.publicationDate = !isNaN(publicationDateTimestamp)
+      const publicationDate = !isNaN(publicationDateTimestamp)
         ? publicationDateTimestamp.toString()
         : '';
-      const langString = v.lang as string;
-      v.lang = langString.split(',');
-      return v;
+      const lang = v.lang.split(',');
+      const id = hashCode(v.reportUrl);
+      return {
+        id,
+        ...v,
+        reportDate,
+        publicationDate,
+        lang,
+      };
     });
     return {
+      page,
+      per_page: this.perPage,
+      total_pages: this.totalPages,
+      total: this.total,
       data: sliced,
-      skip,
-      total,
     };
   }
 }
